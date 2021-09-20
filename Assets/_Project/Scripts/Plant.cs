78e62d498793466de96631a6e0be49aa8d297d23
Bash <sebastiankurg@gmail.com>
2021-09-20 00:06:04 -0400

    First person character controller

---
+++ Assets/_Project/Scripts/Plant.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    private void Update()
+    {
+        // spawn a plant on left click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Plant a plant! :)");
+        }
+    }
+}
