4c53c240cf1e00fc2b52c4ef5cfdd5599bad8769
Joshua Byron <joshuabyron61@yahoo.com>
2021-09-21 01:20:31 -0400

    stressbar mostly functional

---
+++ Assets/_Project/Scripts/Fillamount.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class Fillamount : MonoBehaviour
+{
+    public Image StressBar;
+    public Image HealthCap;
+    public float waitTime = 30.0f;
+    private bool lerp = false;
+    private float newTarget = 0.0f;
+
+    void Start()
+    {
+        StressBar.fillAmount = 0;
+        lerpFill(0.85f);
+    }
+
+     void lerpFill(float newAmmount)
+     {
+        lerp = true;
+        newTarget = newAmmount;
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lerp && StressBar.fillAmount >= newTarget)
+        {
+            lerp = false;
+        }
+
+        if (lerp == true)
+        {
+            StressBar.fillAmount = Mathf.Lerp(StressBar.fillAmount, newTarget, Time.deltaTime/2);
+        }
+
+        if (StressBar.fillAmount < 1-HealthCap.fillAmount)
+        {
+            //StressBar.fillAmount += 1.0f / waitTime * Time.deltaTime;
+        }
+        else if (StressBar.fillAmount > 1-HealthCap.fillAmount)
+        {
+            Debug.Log("Over Stressed!");
+        }
+    }
+}
